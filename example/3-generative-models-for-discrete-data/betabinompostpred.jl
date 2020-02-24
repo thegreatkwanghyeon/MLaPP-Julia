72dacfb7d4b3951f9433becd6060a321454b2254
Kwanghyeon Jeong <sivistys@gmail.com>
2020-02-25 03:21:42 +0900

    Add figure 3-7

---
+++ example/3-generative-models-for-discrete-data/betabinompostpred.jl
@@ -0,0 +1,32 @@
+using Distributions
+
+priorA = 2
+priorB = 2
+n1 = 3
+n2 = 17
+pointX = 0:10
+
+bar1 = bar(
+    pdf.(BetaBinomial(10, priorA + n1, priorB + n2), pointX),
+    title = "Posterior predictive",
+    xticks = 0:10,
+    xlims = (0, 10),
+    yticks = 0:0.05:0.35,
+    ylims = (0, 0.35),
+    grid = false,
+    legend = false
+)
+
+bar2 = bar(
+    pdf.(Binomial(10, (n1 + 1) / (n1 + n2 + 2)), pointX),
+    title = "Plugin predictive",
+    xticks = 0:10,
+    xlims = (0, 10),
+    yticks = 0:0.05:0.35,
+    ylims = (0, 0.35),
+    grid = false,
+    legend = false
+)
+
+result = plot(bar1, bar2, grid = 2, size=(1000, 600))
+png(result, "Figure-3-7")
